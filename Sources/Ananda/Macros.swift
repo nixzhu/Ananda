a0ce17290ade9368f4f1cd9ef96a96293169b705
nixzhu <zhuhongxu@gmail.com>
2023-09-28 15:36:10 +0800

    simple docs

--- Sources/Ananda/Macros.swift
+++ Sources/Ananda/Macros.swift
@@ -1,15 +1,18 @@
+/// Generate initializer for an AnandaModel
 @attached(member, names: named(init(json:)))
 public macro AnandaInit() = #externalMacro(
     module: "AnandaMacros",
     type: "AnandaInitMacro"
 )
 
+/// Provides a raw key for decoding the property
 @attached(peer)
 public macro AnandaKey(_: String) = #externalMacro(
     module: "AnandaMacros",
     type: "AnandaKeyMacro"
 )
 
+/// Ignore this property from decoding
 @attached(peer)
 public macro AnandaIgnored() = #externalMacro(
     module: "AnandaMacros",
