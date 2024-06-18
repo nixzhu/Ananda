4b6bcd3e5bfb2ba72c92b13ef3c0c33285143769
nixzhu <zhuhongxu@gmail.com>
2024-06-18 14:48:18 +0800

    all platforms

--- Package.swift
+++ Package.swift
@@ -4,14 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "Ananda",
-    platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13),
-        .tvOS(.v13),
-        .watchOS(.v6),
-        .macCatalyst(.v13),
-        .visionOS(.v1),
-    ],
     products: [
         .library(
             name: "Ananda",
