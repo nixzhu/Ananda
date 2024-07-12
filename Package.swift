ed7d361dd1eef6d5628a59a879bfef0dba8ed8ce
nixzhu <zhuhongxu@gmail.com>
2024-07-12 21:30:05 +0800

    use yyjson 0.10.0

--- Package.swift
+++ Package.swift
@@ -6,14 +6,14 @@ import PackageDescription
 let packageDependencies: [Package.Dependency] = [
     .package(
         url: "https://github.com/ibireme/yyjson.git",
-        from: "0.9.0"
+        from: "0.10.0"
     ),
 ]
 #else
 let packageDependencies: [Package.Dependency] = [
     .package(
         url: "https://github.com/ibireme/yyjson.git",
-        from: "0.9.0"
+        from: "0.10.0"
     ),
     .package(
         url: "https://github.com/michaeleisel/JJLISO8601DateFormatter.git",
